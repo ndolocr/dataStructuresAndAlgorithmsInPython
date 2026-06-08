1a9181d75d2c3081ef983084cb21771e17edade2
raphaelndolo <raphael.ndolo@equitybank.co.ke>
2026-06-08 14:39:02 +0300

    Review Linked List pre Append.

--- LinkedList/QuestionThree_PreAppend.py
+++ LinkedList/QuestionThree_PreAppend.py
@@ -2,42 +2,61 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-    
-class LiknedList:
+
+class LinkedList:
     length = 0
     def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
-        LiknedList.length += 1
-    
+        LinkedList.length += 1
+
     def print_linked_list(self):
         temp = self.head
-        print(f"Linked List Length --: {LiknedList.length}")
+
+        print(f"Linked List Length --: {LinkedList.length}")
+        print("Linked List:- ", end=" ")
         while temp is not None:
             print(f"({temp.value}) -> ", end="")
             temp = temp.next
         print("None")
-    
+
     def append(self, value):
-        # Check to see if Linked List is Empty
         new_node = Node(value)
+
         if self.head == None:
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.next = new_node
             self.tail = new_node
-        LiknedList.length += 1
+        LinkedList.length += 1
         return True
     
+    def pre_append(self, value):
+        new_node = Node(value)
+
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        LinkedList.length += 1
+        return True
+
 def main():
-    my_linked_list = LiknedList(0)
-    my_linked_list.append(1)
-    my_linked_list.append(2)
-    my_linked_list.append(3)
-    my_linked_list.append(4)
+    my_linked_list = LinkedList(4)
+
     my_linked_list.append(5)
+    my_linked_list.append(6)
+    my_linked_list.append(7)
+    my_linked_list.append(8)
+    my_linked_list.append(9)
+
+    my_linked_list.pre_append(3)
+    my_linked_list.pre_append(2)
+    my_linked_list.pre_append(1)
 
     my_linked_list.print_linked_list()
 
