9e2137906f6662e90d176effc13bba9b22088c35
raphaelndolo <raphael.ndolo@equitybank.co.ke>
2026-06-12 13:05:42 +0300

    Inititate Binary Search Tree

---
+++ Trees/QuestionOne_Inititation.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+def main():
+    print("INITITATE BINARY SEARCH TREE")
+    my_tree = BinarySearchTree()
+
+if __name__ == "__main__":
+    main()
