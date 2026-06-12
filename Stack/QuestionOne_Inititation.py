9b55585f0d0bb5baf569ac117bda0ff26fd0ef24
raphaelndolo <raphael.ndolo@equitybank.co.ke>
2026-06-12 08:13:38 +0300

    Inititating a Stack

---
+++ Stack/QuestionOne_Inititation.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.top = new_node
+        self.length = 1
