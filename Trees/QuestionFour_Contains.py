76c8d68731228aa5e15eb9e240dbd9d89cde832d
raphaelndolo <raphael.ndolo@equitybank.co.ke>
2026-06-12 17:32:50 +0300

    Contains function in Tree structure

---
+++ Trees/QuestionFour_Contains.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+
+        if self.root is None:
+            self.root = new_node
+            return new_node
+        
+        temp = self.root
+        while(True):
+            if new_node.value == temp.value:
+                return False
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return new_node
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return new_node
+                temp = temp.right
+
+    def contains(self, value):
+        if self.root is None:
+            return False
+        temp = self.root
+        while temp is not None:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return temp
+        return None
